1907fd38e5e812065eecade023b066bfcd34f659
padrecedano <padre.cedano@gmail.com>
2023-03-27 15:14:45 +0200

    v. 2023.1 Versión offline: Optimización de relaciones entre entidades y otros

---
+++ app/src/main/java/org/deiverbum/app/data/entity/relation/LHOfficePatristicWithAll.java
@@ -0,0 +1,41 @@
+package org.deiverbum.app.data.entity.relation;
+
+import androidx.room.Embedded;
+import androidx.room.Relation;
+
+import org.deiverbum.app.data.entity.HomilyEntity;
+import org.deiverbum.app.data.entity.LHOfficePatristicEntity;
+import org.deiverbum.app.data.entity.LHResponsoryEntity;
+import org.deiverbum.app.model.LHOfficePatristic;
+
+/**
+ * @author A. Cedano
+ * @version 1.0
+ * @since 2023.1
+ */
+public class LHOfficePatristicWithAll {
+    @Embedded
+    public LHOfficePatristicEntity lhPatristica;
+
+    @Relation(
+            parentColumn = "homilyFK",
+            entityColumn = "homilyID",
+            entity = HomilyEntity.class
+    )
+    public HomilyAll homiliaAll;
+
+    @Relation(
+            parentColumn = "responsoryFK",
+            entityColumn = "responsoryID",
+            entity = LHResponsoryEntity.class
+    )
+    public LHResponsoryEntity lhResponsorio;
+
+
+    public LHOfficePatristic getDomainModel(Integer timeId) {
+        LHOfficePatristic theModel= homiliaAll.getPatristicaDomainModel();
+        theModel.setTheme(lhPatristica.tema);
+        theModel.setResponsorioLargo(lhResponsorio.getDomainModel(timeId));
+        return theModel;
+    }
+}
