89ccdd2e1dece02c58f8714f26a668fbf755eae7
padrecedano <padre.cedano@gmail.com>
2023-09-16 15:40:19 +0200

    v. 2023.1.3 Migración parcial a Kotlin y mejora de la arquitectura (9)

--- app/src/main/java/org/deiverbum/app/data/database/AppDatabase.kt
+++ app/src/main/java/org/deiverbum/app/data/database/AppDatabase.kt
@@ -2,6 +2,7 @@ package org.deiverbum.app.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import org.deiverbum.app.data.database.dao.TodayDao
 import org.deiverbum.app.data.entity.*
 
 /**
@@ -10,13 +11,11 @@ import org.deiverbum.app.data.entity.*
  * @since 2023.1
  */
 @Database(
-    entities = [TodayEntity::class, LiturgyEntity::class, LiturgyTimeEntity::class, LiturgySaintJoinEntity::class, SaintEntity::class, SaintLifeEntity::class, SaintShortLifeEntity::class, LHInvitatoryEntity::class, LHInvitatoryJoinEntity::class, LHHymnEntity::class, LHHymnJoinEntity::class, LHPsalmodyEntity::class, LHPsalmodyJoinEntity::class, PsalmEntity::class, LHAntiphonEntity::class, LHThemeEntity::class, EpigraphEntity::class, LHOfficeVerseEntity::class, LHOfficeVerseJoinEntity::class, LHOfficeBiblicalJoinEntity::class, LHOfficeBiblicalEntity::class, LHOfficeBiblicalEasterEntity::class, LHOfficePatristicEntity::class, LHOfficePatristicJoinEntity::class, HomilyEntity::class, PaterEntity::class, PaterOpusEntity::class, LHResponsoryEntity::class, LHResponsoryShortEntity::class, BiblieBookEntity::class, BibleReadingEntity::class, LHReadingShortEntity::class, LHReadingShortJoinEntity::class, LHGospelCanticleEntity::class, LHIntercessionsEntity::class, LHIntercessionsJoinEntity::class, PrayerEntity::class, LHPrayerEntity::class, LiturgyHomilyJoinEntity::class, BibleHomilyJoinEntity::class, BibleHomilyThemeEntity::class, LiturgyGroupEntity::class, LiturgyColorEntity::class, MassReadingEntity::class, MassReadingJoinEntity::class, SyncStatusEntity::class, DBTableEntity::class,
-               KyrieEntity::class,VirginAntiphonEntity::class, NightPrayerEntity::class],
-    version = 1
+    entities = [TodayEntity::class, LiturgyEntity::class, LiturgyTimeEntity::class, LiturgySaintJoinEntity::class, SaintEntity::class, SaintLifeEntity::class, SaintShortLifeEntity::class, LHInvitatoryEntity::class, LHInvitatoryJoinEntity::class, LHHymnEntity::class, LHHymnJoinEntity::class, LHPsalmodyEntity::class, LHPsalmodyJoinEntity::class, PsalmEntity::class, LHAntiphonEntity::class, LHThemeEntity::class, EpigraphEntity::class, LHOfficeVerseEntity::class, LHOfficeVerseJoinEntity::class, LHOfficeBiblicalJoinEntity::class, LHOfficeBiblicalEntity::class, LHOfficeBiblicalEasterEntity::class, LHOfficePatristicEntity::class, LHOfficePatristicJoinEntity::class, HomilyEntity::class, PaterEntity::class, PaterOpusEntity::class, LHResponsoryEntity::class, LHResponsoryShortEntity::class, BibleBookEntity::class, BibleReadingEntity::class, LHReadingShortEntity::class, LHReadingShortJoinEntity::class, LHGospelCanticleEntity::class, LHIntercessionsEntity::class, LHIntercessionsJoinEntity::class, PrayerEntity::class, LHPrayerEntity::class, LiturgyHomilyJoinEntity::class, BibleHomilyJoinEntity::class, BibleHomilyThemeEntity::class, LiturgyGroupEntity::class, LiturgyColorEntity::class, MassReadingEntity::class, MassReadingJoinEntity::class, SyncStatusEntity::class, DBTableEntity::class,
+               KyrieEntity::class,LHKyrieJoinEntity::class,VirginAntiphonEntity::class, LHVirginAntiphonJoinEntity::class,LHNightPrayerEntity::class],
+    version = 2
 )
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun todayDao(): org.deiverbum.app.data.db.dao.TodayDao
-    abstract fun prayDao(): org.deiverbum.app.data.database.dao.TodayDao
-
+    abstract fun todayDao(): TodayDao
 }
 
